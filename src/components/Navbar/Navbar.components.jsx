171409bec1eda439609334d0c8b8a84557fe93b5
Irfan Asif <hashtag.irfan@gmail.com>
2021-09-28 09:28:56 +0530

    commit

--- src/components/Navbar/Navbar.components.jsx
+++ src/components/Navbar/Navbar.components.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BiChevronRight, BiSearch } from 'react-icons/bi';
+import logo from './masterlogo.png';
 
 const NavSm = () => {
   return (
@@ -20,37 +21,33 @@ const NavSm = () => {
 };
 const NavMd = () => {
   return (
-
-      <div className='w-full flex items-center bg-white gap-3 px-3 py-2 rounded-sm' >
-        <BiSearch />
-        <input
-          type='search'
-          className='w-full focus:outline-none'
-          placeholder='Search for movies, events, plays, sports and activities'
-        />
-      </div>
-
+    <div className='w-full flex items-center bg-white gap-3 px-3 py-2 rounded-sm'>
+      <BiSearch />
+      <input
+        type='search'
+        className='w-full focus:outline-none'
+        placeholder='Search for movies, events, plays, sports and activities'
+      />
+    </div>
   );
 };
 const NavLg = () => {
   return (
-
-      <div>
-          <div>
-              <div>
-                img   
-              </div>
-              <div className='w-full flex items-center bg-white gap-3 px-3 py-2 rounded-sm' >
-                  <BiSearch />
-                  <input
-                      type='search'
-                      className='w-full focus:outline-none'
-                      placeholder='Search for movies, events, plays, sports and activities'
-                  />
-              </div>
-          </div>
+    <div className='w-full'>
+      <div className='flex items-center w-full'>
+        <div className='w-12 h-12'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='w-full flex items-center bg-white gap-3 px-3 py-2 rounded-sm'>
+          <BiSearch />
+          <input
+            type='search'
+            className='w-full focus:outline-none'
+            placeholder='Search for movies, events, plays, sports and activities'
+          />
+        </div>
       </div>
-
+    </div>
   );
 };
 
