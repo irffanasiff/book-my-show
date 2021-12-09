20937e2a19e19d0659468770971bdfec6a552064
Irfan Asif <hashtag.irfan@gmail.com>
2021-12-09 19:57:18 +0530

    deployment"
    "

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Route } from 'react-router-dom';
 import axios from 'axios';
 import HomePage from './pages/Home.page.jsx';
 import MoviePage from './pages/Movie.page.jsx';
 import DefaultHOC from './HOC/Default.HOC.jsx';
 import MovieHOC from './HOC/Movie.HOC.jsx';
 import PlaysPage from './pages/Plays.pages.jsx';
-
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
